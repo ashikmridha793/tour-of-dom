bfda515b650c40fe7cb75046c5b944500ab5bbee
Ashik Mridha <ashikmridha43@gmail.com>
2026-02-15 17:56:40 +0600

    added some dome with css

---
+++ js/cssInJs.js
@@ -0,0 +1,6 @@
+// const sections = document.getElementById('section');
+const sections = document.querySelectorAll('secton');
+// console.log(sections)
+for(const secton of sections){
+    console.log(secton);
+}
